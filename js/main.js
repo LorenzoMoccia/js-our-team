7439b752a7c528119c73628b71ef8a42050d7111
LorenzoMoccia <moccialorenzo98@gmail.com>
2022-11-07 15:12:55 +0100

    Objects array

--- js/main.js
+++ js/main.js
@@ -1,1 +1,42 @@
 "use strict";
+
+const members = [
+    {
+        firstName: "Wayne",
+        lastName: "Barnett",
+        role: "Founder & CEO",
+        image: "wayne-barnett-founder-ceo.jpg"
+    },
+    {
+        firstName: "Angela",
+        lastName: "Caroll",
+        role: "Chief Editor",
+        image: "angela-caroll-chief-editor.jpg"
+    },
+    {
+        firstName: "Walter",
+        lastName: "Gordon",
+        role: "Office Manager",
+        image: "walter-gordon-office-manager.jpg"
+    },
+    {
+        firstName: "Angela",
+        lastName: "Lopez",
+        role: "Social Media Manager",
+        image: "angela-lopez-social-media-manager.jpg"
+    },
+    {
+        firstName: "Scott",
+        lastName: "Estrada",
+        role: "Developer",
+        image: "scott-estrada-developer.jpg"
+    },
+    {
+        firstName: "Barbara",
+        lastName: "Ramos",
+        role: "Graphic Designer",
+        image: "barbara-ramos-graphic-designer.jpg"
+    },
+
+
+];
